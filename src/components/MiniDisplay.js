14bc3f7a4ee975b08a43d424b7d332769b774f63
si3mshady <cloudwarrior2891@gmail.com>
2021-02-15 09:49:34 -0600

    enhanced mini display with CSS rotation

--- src/components/MiniDisplay.js
+++ src/components/MiniDisplay.js
@@ -7,18 +7,36 @@ export default function MiniDisplay({gps}) {
                 <div className="miniDisplay__container card">
                     
                      <div className="miniDisplay__container--output ">
-                       
+                     <div className="miniDisplay__container--output--front">
+                     
+                                {gps.map((location, index) => 
+                            
+                            (
+                                <>
+                                    <p key={index}>{`Longitude: ${location.longitude !== null? location.longitude: ""}`}</p>
+                                    <p key={index+1}>{`Latitude: ${location.latitude !== null? location.latitude: ""}`}</p>
+                                    <p key={index+2}>{`Accuracy: ${location.accuracy !== null? location.accuracy: ""}%`}</p> 
+                                        
+                                </> ))}
+                     </div>
+
+                     <div className="miniDisplay__container--output--back">
+                    
                      {gps.map((location, index) => 
-                  
-                  (
-                    <>
-                        <p key={index}>{`Longitude: ${location.longitude !== null? location.longitude: ""}`}</p>
-                        <p key={index+1}>{`Latitude: ${location.latitude !== null? location.latitude: ""}`}</p>
-                        <p key={index+2}>{`Accuracy: ${location.accuracy !== null? location.accuracy: ""}%`}</p> 
-                               
-                    </> ))}
+                            
+                            (
+                                <>
+                                    <p key={index}>{`Longitude: ${location.longitude !== null? location.longitude: ""}`}</p>
+                                    <p key={index+1}>{`Latitude: ${location.latitude !== null? location.latitude: ""}`}</p>
+                                    <p key={index+2}>{`Accuracy: ${location.accuracy !== null? location.accuracy: ""}%`}</p> 
+                                        
+                                </> ))}
+                     </div>
+                    
 
                     </div>
+
+                    
                 </div> 
             </div>
     )
