1e6c803eeeaddc5741cc75c6202c933c11770902
si3mshady <cloudwarrior2891@gmail.com>
2021-02-21 11:33:28 -0600

    deleted duplicate import of reverseGeolocation lib from minidisplay.js

--- src/components/MiniDisplay.js
+++ src/components/MiniDisplay.js
@@ -3,7 +3,7 @@ import React from 'react'
 import '../index.css';
 
 export default function MiniDisplay({gps,labels, reverseGeoLocation}) {
-    const [geolocation, resolveGeolocation] = React.useState()
+    
  
     return (
         <div className="miniDisplay">
